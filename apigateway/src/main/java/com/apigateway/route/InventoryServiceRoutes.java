cb3b9838d9dc2e8c12e2975acfdbfc41850f1226
AashSK <49270873+AashSK@users.noreply.github.com>
2026-07-25 08:32:19 +0000

    Rerouting for InventoryService implemented & Ports for services made internal

--- apigateway/src/main/java/com/apigateway/route/InventoryServiceRoutes.java
+++ apigateway/src/main/java/com/apigateway/route/InventoryServiceRoutes.java
@@ -1,5 +1,24 @@
 package com.apigateway.route;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
 public class InventoryServiceRoutes {
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryRoute() {
+        return route("inventory-service")
+                .GET("/api/v1/inventory/**", http())
+                .PUT("/api/v1/inventory/**", http())
+                .before(uri("http://localhost:8080"))
+                .build();
+    }
 
 }
